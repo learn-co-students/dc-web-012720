c47efe3b0649182a42643df000cf5df65651ed7d
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2020-02-18 12:53:22 -0500

    post lecture 16

--- 16-intro-to-sinatra/app/controllers/application_controller.rb
+++ 16-intro-to-sinatra/app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < Sinatra::Base
 
+    set(:views, "app/views")
+
+    get "/" do 
+        erb :homepage
+    end
+
+    get "/books" do 
+        # show all the books
+        @books = Book.all 
+        erb :index
+    end
+
+    get "/books/:id" do 
+        id = params[:id]
+        @book = Book.find(id)
+        erb :show
+
+    end
+
 end
