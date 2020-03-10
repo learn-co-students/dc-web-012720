d40473dac5b7cde60d5b0c1eabd3d1dbd2940006
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2020-03-10 09:50:40 -0400

    stuff

---
+++ 31-scope-and-stuff/script.js
@@ -0,0 +1,18 @@
+
+function favoriteTeacher() {
+    teacher = "Snape"
+    if (teacher) {
+        console.log(`My favorite professor is ${teacher}`)
+    } else {
+        console.log("I haven't chosen a favorite!")
+    }
+}
+
+
+function something() {
+    if (teacher) {
+        console.log(`My favorite professor is ${teacher}`)
+    } else {
+        console.log("I haven't chosen a favorite!")
+    }
+}
