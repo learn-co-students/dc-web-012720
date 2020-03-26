8c245963f3b53bd4a651d9f4cb72e78000d7f3e2
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2020-03-26 15:57:50 -0400

    post lecture 39

--- 39-this/script.js
+++ 39-this/script.js
@@ -1,23 +1,34 @@
+"use strict"
+
 console.log(self)
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log('DOM loaded')
-    let button = document.querySelector('button')
-    button.addEventListener('click', () => console.log('clicked'))
+    let annButton = document.querySelector('#ann')
+    annButton.addEventListener('click', ann.greet)
+    let paulButton = document.querySelector('#paul')
+    paulButton.addEventListener('click', greet.bind(paul))
 })
+
 
 function greet() {
     debugger
     console.log(`${this.firstName} says hi`)
+    function setNewProperty() {
+        this.newProperty = "foobar"
+    }
+    setNewProperty()
 }
 
 let paul = {
     firstName: "Paul",
-    lastName: "Nicholsen"
+    lastName: "Nicholsen",
+    greet: greet
 }
 
 let ann = {
     firstName: "Ann",
-    lastName: "Duong"
+    lastName: "Duong",
+    greet: greet
 }
 
